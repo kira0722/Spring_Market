fb329641b46c68e366752ea19371e59602b9e8d1
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-22 12:30:14 -0500

    subiendo finalizacion del crud repository de producto, terminando todos los metodos para la clase

--- src/main/java/com/example/market/persistence/ProductRepository.java
+++ src/main/java/com/example/market/persistence/ProductRepository.java
@@ -2,10 +2,12 @@ package com.example.market.persistence;
 
 import com.example.market.persistence.crud.ProductoCrudRepository;
 import com.example.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +21,17 @@ public class ProductRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad, boolean estado){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
