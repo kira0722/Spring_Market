17518374f94d87048ec06b00fed08b5d65c3e700
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-22 10:37:04 -0500

    subiendo creacion de las clases entidades segun la BD

---
+++ src/main/java/com/example/market/persistence/entity/cliente.java
@@ -0,0 +1,28 @@
+package com.example.market.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class cliente {
+
+    @Id
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Integer celular;
+
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+
+}
