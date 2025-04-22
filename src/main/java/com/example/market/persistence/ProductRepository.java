2b49916f56afcbf28adfff2c8acaa59590292e4e
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-22 12:21:43 -0500

    subiendo query methods para productos y manejarlos en el repository de product

--- src/main/java/com/example/market/persistence/ProductRepository.java
+++ src/main/java/com/example/market/persistence/ProductRepository.java
@@ -4,11 +4,20 @@ import com.example.market.persistence.crud.ProductoCrudRepository;
 import com.example.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getall(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad, boolean estado){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/example/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/example/market/persistence/crud/ProductoCrudRepository.java
@@ -2,8 +2,16 @@ package com.example.market.persistence.crud;
 
 
 import com.example.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+//    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
